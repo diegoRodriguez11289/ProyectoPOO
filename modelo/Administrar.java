d800a4b42a347edf3c3485b06c31a9130bd22e13
diegoRodriguez11289 <95194926+diegoRodriguez11289@users.noreply.github.com>
2021-11-29 23:58:31 -0500

    Update Administrar.java

--- modelo/Administrar.java
+++ modelo/Administrar.java
@@ -23,7 +23,26 @@ public class Administrar{
     dueños= new ArrayList<>();
     DueñoDeMascota dueño1=new DueñoDeMascota("Diego Adriel","Los olivos","0994047722",Guayaquil,"diadrodr@espol.edu.ec","Rodriguez Flores","0924781081");
     DueñoDeMascota dueño2=new DueñoDeMascota("Pedro Carlos","Mi casa","0993038157",Guayaquil,"carlos2@gmail.com","Rodriguez Serra","0945308110");
+    DueñoDeMascota dueño3=new DueñoDeMascota("Rodrigo Andres","Sauces 5 mz109 s10","0994047722",Guayaquil,"Rodrigo7@espol.edu.ec","Torres Valladares","0724788456");
+    DueñoDeMascota dueño4=new DueñoDeMascota("Josue Cesar","Sauces 5 mz109 s11","0981234568",Guayaquil,"Josue8@espol.edu.ec","Alcivar Velasquez","0427676123");
+    DueñoDeMascota dueño5=new DueñoDeMascota("John Piero","Barrio aniñado de Quito","0994047722",Quito,"jlopez1@espol.edu.ec","Lopez Sorroza","0169781276");
+    DueñoDeMascota dueño6=new DueñoDeMascota("Carlos Andres","Barrio podre de Cuenca","0982345671",Cuenca,"Cflor34@espol.edu.ec","Flores Moncayo","0871235678");
+    DueñoDeMascota dueño7=new DueñoDeMascota("David Jesus","Sauces 5 mz109 s13","0999876543",Guayaquil,"david17@espol.edu.ec","Vasques Almeida","1023457126");
+    DueñoDeMascota dueño8=new DueñoDeMascota("Piero Harry","Samanes 3 mz123 s10","0993346689",Guayaquil,"Ppotter68@espol.edu.ec","Hincapie Potter","0341236987");
+    DueñoDeMascota dueño9=new DueñoDeMascota("Antonio Walter","Preguntale a mi mama ","0988977724",Quito,"AV25@hotmail.com","Valencia Espinoza","0724788456");
+    DueñoDeMascota dueño10=new DueñoDeMascota("Adolf Alejandro","Alborada 3ra etapa maz30 S27","0992431974",Guayaquil,"AdolfH@gmail.com","Quizhpe Guartanga","0913456782");
+
+    
     dueños.add(dueño1);
+    dueños.add(dueño2);
+    dueños.add(dueño3);
+    dueños.add(dueño4);
+    dueños.add(dueño5);
+    dueños.add(dueño6);
+    dueños.add(dueño7);
+    dueños.add(dueño8);
+    dueños.add(dueño9);
+    dueños.add(dueño10);
     
   }
 
